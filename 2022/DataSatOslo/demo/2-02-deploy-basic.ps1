8e365054a7f9996002b53d9d2cc97ebf0af1e18b
Kamil Nowinski <kamil@nowinski.net>
2022-10-02 17:09:18 +0100

    DataSatOslo2022

---
+++ 2022/DataSatOslo/demo/2-02-deploy-basic.ps1
@@ -0,0 +1,16 @@
+#$SubscriptionName = 'MVP'
+#Set-AzContext -Subscription $SubscriptionName
+$ResourceGroupName = 'rg-datafactory'
+$DataFactoryName = "BigFactoryTest"
+$Location = "UKSouth"
+$RootFolder = "D:\GitAz\SQLPlayer\ADF-demo\BigFactoryTest"
+
+# Dry Run (WhatIf)
+Publish-AdfV2FromJson -RootFolder "$RootFolder" -ResourceGroupName "$ResourceGroupName" `
+    -DataFactoryName "$DataFactoryName" -Location "$Location" -DryRun
+
+# Real run
+Publish-AdfV2FromJson -RootFolder "$RootFolder" -ResourceGroupName "$ResourceGroupName" `
+    -DataFactoryName "$DataFactoryName" -Location "$Location"
+
+
